73a8ef103ae108f19e24ae48dc1af720885705bd
Puma Release Smoke <smoke@example.test>
2026-03-23 14:04:51 +0900

    Release v7.3.0

--- lib/puma/const.rb
+++ lib/puma/const.rb
@@ -100,8 +100,8 @@ module Puma
   # too taxing on performance.
   module Const
 
-    PUMA_VERSION = VERSION = "7.2.0"
-    CODE_NAME = "On The Corner"
+    PUMA_VERSION = VERSION = "7.3.0"
+    CODE_NAME = "INSERT CODENAME HERE"
 
     PUMA_SERVER_STRING = ["puma", PUMA_VERSION, CODE_NAME].join(" ").freeze
 
